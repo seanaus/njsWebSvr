33b6dd65ba72e02b77eaa8c69be6576a325d4893
seanaus <seanaus1972@gmail.com>
2022-12-22 17:29:49 +0000

    dotEnv config now working and wrapped into config.js

--- app.js
+++ app.js
@@ -1,29 +1,28 @@
 "use strict";
-const PORT = 8080;
 const express = require("express");
 const app = express();
-// const env = require("dotenv").config();
-// const router = express.Router();
+const config = require("./config");
 const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 const productRoutes = require("./routes/product");
 const cartRoutes = require("./routes/cart");
-const auth = require("./core/auth");
 const methodOverride = require("method-override");
 const { connect } = require("./middleware/auth");
 
 // Middleware
-app.use(connect);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.set('view engine', 'ejs');
+
+// Custom Middleware
+app.use(connect);
 app.use("/", authRoutes.routes);
 app.use("/", userRoutes.routes);
 app.use("/", productRoutes.routes);
 app.use("/", cartRoutes.routes);
 
-app.set('view engine', 'ejs');
-
-app.listen(PORT, () => {
-  console.log(`App listening on ${PORT}.......`);
+app.listen(config.port, () => {
+  console.log(`App listening on ${config.port}.......`);
+  // config.log();
 });
--- config.js
+++ config.js
@@ -1,17 +1,77 @@
 "use strict";
+require("dotenv").config();
 const PORT = process.env.PORT;
 const HOST = process.env.HOST;
-const HOST_URL = `http://${process.env.HOST}:${process.env.PORT}`;
+const URL = `http://${process.env.HOST}:${process.env.PORT}`;
+const PRODUCTION = process.env.PRODUCTION;
+const FIREBASE_CONFIG = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+  measurementId: process.env.MEASSUREMENT_ID
+};
+const log = ()=> {
+  console.log(`port: ${PORT}`);
+  console.log(`host: ${HOST}`);
+  console.log(`url: ${URL}`);
+  console.log(`apikey: ${FIREBASE_CONFIG.apiKey}`);
+  console.log(`authDomain: ${FIREBASE_CONFIG.authDomain}`);
+  console.log(`databaseURL: ${FIREBASE_CONFIG.databaseURL}`);
+  console.log(`projectId: ${FIREBASE_CONFIG.projectId}`);
+  console.log(`storageBucket: ${FIREBASE_CONFIG.storageBucket}`);
+  console.log(`messagingSenderId: ${FIREBASE_CONFIG.messagingSenderId}`);
+  console.log(`appId: ${FIREBASE_CONFIG.appId}`);
+  console.log(`measurementId: ${FIREBASE_CONFIG.measurementId}`);
+  console.log(`production: ${PRODUCTION}`);
+}
+module.exports = {
+  port: PORT,
+  host: HOST,
+  url: URL,
+  firebaseConfig: FIREBASE_CONFIG,
+  production: PRODUCTION,
+  log
+}
 
-const API_KEY = process.env.API_KEY;
-const AUTH_DOMAIN = process.env.AUTH_DOMAIN;
-const DATABASE_URL = process.env.DATABASE_URL;
-const PROJECT_ID = process.env.PROJECT_ID;
-const STORAGE_BUCKET = process.env.STORAGE_BUCKET;
-const MESSAGING_SENDER_ID = process.env.MESSAGING_SENDER_ID;
-const APP_ID = process.env.APP_ID;
-const MEASSUREMENT_ID = process.env.MEASSUREMENT_ID;
-const PRODUCTION = process.env.production;
+// module.exports = {
+//   port: process.env.PORT,
+//   host: process.env.HOST,
+//   url: `http://${process.env.HOST}:${process.env.PORT}`,
+//   firebaseConfig: {
+//     apiKey: process.env.API_KEY,
+//     authDomain: process.env.AUTH_DOMAIN,
+//     databaseURL: process.env.DATABASE_URL,
+//     projectId: process.env.PROJECT_ID,
+//     storageBucket: process.env.STORAGE_BUCKET,
+//     messagingSenderId: process.env.MESSAGING_SENDER_ID,
+//     appId: process.env.APP_ID,
+//     measurementId: process.env.MEASSUREMENT_ID
+//   },
+//   production: process.env.PRODUCTION,
+// };
+
+
+
+/* "use strict";
+const env = require("dotenv").config();
+
+const PORT = env.PORT;
+const HOST = env.HOST;
+const HOST_URL = env.HOST_URL;
+
+const API_KEY = env.API_KEY;
+const AUTH_DOMAIN = env.AUTH_DOMAIN;
+const DATABASE_URL = env.DATABASE_URL;
+const PROJECT_ID = env.PROJECT_ID;
+const STORAGE_BUCKET = env.STORAGE_BUCKET;
+const MESSAGING_SENDER_ID = env.MESSAGING_SENDER_ID;
+const APP_ID = env.APP_ID;
+const MEASSUREMENT_ID = env.MEASSUREMENT_ID;
+const PRODUCTION = env.production;
 
 module.exports = {
   port: PORT,
@@ -29,3 +89,4 @@ module.exports = {
   },
   production: PRODUCTION
 };
+ */

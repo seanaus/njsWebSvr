66928bc694f042aef72312e9d5e3c8c447805273
seanaus <94388567+seanaus@users.noreply.github.com>
2023-03-22 17:31:05 +0000

    navBar Dev

--- public/js/navBar.js
+++ public/js/navBar.js
@@ -3,17 +3,17 @@ const toggle = () => {
     const navPills = navBar.getElementsByClassName("navPill");
     for (let idx = 0; idx < navPills.length; idx++) {
         // let isCollapsed = navPills[i].classList.contains("isCollapsed");
-        if(navPills[idx].classList.contains("isCollapsed")) {
+        if (navPills[idx].classList.contains("isCollapsed")) {
             navPills[idx].classList.remove("isCollapsed");
             // const children = navPills[idx].getElementsByTagName("div");
             // for (let cIdx = 0; cIdx < children.length; cIdx++) {
-                // children[cIdx].classList.remove("isCollapsed");
-                // console.log(`CHILD: ${children[cIdx]}`)
-                // children[cIdx].style.visibility = "visible";
+            // children[cIdx].classList.remove("isCollapsed");
+            // console.log(`CHILD: ${children[cIdx]}`)
+            // children[cIdx].style.visibility = "visible";
             // }
-           // navBar.style.display = "none";
+            // navBar.style.display = "none";
         } else {
-            navPills[idx].classList.add("isCollapsed"); 
+            navPills[idx].classList.add("isCollapsed");
             // navBar.style.display = "flex";
             // const children = navPills[idx].getElementsByTagName("div");
             // for (let cIdx = 0; cIdx < children.length; cIdx++) {
@@ -24,6 +24,26 @@ const toggle = () => {
         }
     }
 
+    // setTimeout(() => {
+    //     for (let idx = 0; idx < navPills.length; idx++) {
+    //         if (navPills[idx].style.opacity === '0') {
+    //             navPills[idx].style.opacity = '1';
+    //         } else {
+    //             navPills[idx].style.opacity = '0';
+    //         }
+    //     }
+    // }, 500)
+
+    // for (let idx = 0; idx < navPills.length; idx++) {
+    //     const color = navPills[idx].style.color;
+    //     // const color2 = navPills[idx].style.backgroundColor; 
+
+    //     if (navPills[idx].style.opacity === '0') {
+    //         navPills[idx].style.opacity = '1';
+    //     } else {
+    //         navPills[idx].style.opacity = '0';
+    //     }
+    // }
     // for (let idx = 0; idx < navPills.length; idx++) {
     //     const children = navPills[idx].getElementsByTagName("div");
     //     for (let cIdx = 0; cIdx < children.length; cIdx++) {
